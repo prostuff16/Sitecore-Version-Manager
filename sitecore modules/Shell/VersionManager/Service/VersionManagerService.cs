0961fbc788f09c6e653aeacc400f4426a56feae1
khiltner <khiltner@mihomes.com>
2022-02-03 21:33:07 -0500

    - Logging cleanup

--- sitecore modules/Shell/VersionManager/Service/VersionManagerService.cs
+++ sitecore modules/Shell/VersionManager/Service/VersionManagerService.cs
@@ -10,14 +10,14 @@ namespace Sitecore.VersionManager.sitecore_modules.Shell.VersionManager.Service
 
         public void Run()
         {
-            Log.Info($"Starting Version cleanup","SitecoreVersionManager");
+            Log.Info($"Starting versions cleanup","SitecoreVersionManager");
             var items = Sitecore.VersionManager.VersionManager.ItemVersions;
 
             foreach (var item in items)
             {
                 Sitecore.VersionManager.VersionManager.DeleteItemVersions(item);
             }
-            Log.Info($"Finished Version cleanup", "SitecoreVersionManager");
+            Log.Info($"Finished versions cleanup for {items.Count}", "SitecoreVersionManager");
         }
     }
 }
